7b6096e4b503dd8d0dd16dc427a75fc1a55e902a
JorgePerez3 <jorge.perez33@upr.edu>
2024-10-16 17:09:51 -0400

    Added funxctions for attack, defend and status

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+const port = 3001;
 
-PORT = 3000;
+app.use(cors());
+app.use(express.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+let status = 'bad'; 
+
+app.get('/status', (req, res) => {
+  console.log('GET /status called');
+  res.json({ status });
 });
 
-app.listen(3000, () => {
-    console.log(`Litsening on port ${PORT}.`);
+app.post('/start', (req, res) => {
+  console.log('POST /start called');
+  status = 'bad';
+  res.json({ message: 'Attack started', status });
+});
+
+app.post('/defend', (req, res) => {
+  console.log('POST /defend called');
+  status = 'good'; 
+  res.json({ message: 'Defending attack', status });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
